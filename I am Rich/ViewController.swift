612387555e891f4fc71fb931b597e11e74706b21
Kushagra Sharma <sharmakushagra3125@gmail.com>
2020-09-06 23:42:47 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kushagra Sharma on 06/09/20.
+//  Copyright © 2020 Kushagra Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
